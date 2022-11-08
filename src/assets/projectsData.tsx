5990ed2617a42f06e452538ae961d29ebc628d06
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-11-08 11:45:13 +0100

    adds projectsData file

---
+++ src/assets/projectsData.tsx
@@ -0,0 +1,22 @@
+export const projectsData = [
+    {
+        "projectName": "Green forests of Atlantico",
+        "description": "Description of Green forests"
+    },
+    {
+        "projectName": "Green seagrass of Bahamas",
+        "description": "Description of Green seagrass"
+    },
+    {
+        "projectName": "Reforestation of Brazil",
+        "description": "Description of Reforestation"
+    },
+    {
+        "projectName": "Blue ocean of Atlantico",
+        "description": "Description of Blue Ocean"
+    },
+    {
+        "projectName": "Blue seagrass of Bahamas",
+        "description": "Description of Blue seagrass"
+    },
+]
